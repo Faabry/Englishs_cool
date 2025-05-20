b6147f437c716d673e91e296927f86af58be7d06
Airton Rafael Fabre <airton.fabre@labsoft.com.br>
2025-05-20 06:47:05 -0300

    Criação do perfil de usuário e adequações nos toggle items

--- app_learn_pics/urls.py
+++ app_learn_pics/urls.py
@@ -3,10 +3,12 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls), 
     path('', views.category_selection, name='category_selection'),
+    path('profile/', views.profile_view, name='profile'),
     path('<str:category>/', views.show_random_image, name='show_image'),
-    path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/signup/', views.signup, name='signup'),
-    path('accounts/logout/', views.custom_logout, name='logout'),  # <-- Use /accounts/logout/ and place it AFTER the include
+    
+    # path('accounts/logout/', views.custom_logout, name='logout'),  # <-- Use /accounts/logout/ and place it AFTER the include
 ]
